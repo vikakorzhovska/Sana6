1471195c056fc0fafe7284b6bd27be03a4f6417a
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2024-01-06 12:15:46 +0200

    Created class Student

---
+++ Library/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Student : People
+    {
+        public int Course { get; set; }
+        public string Group { get; set; }
+        public string Faculty { get; set; }
+        public string University { get; set; }
+        public Student(string firstName, string lastName, DateTime dateOfBirth, int course, string group, string faculty, string university) : base(firstName, lastName, dateOfBirth)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            University = university;
+        }
+        public override string ShowInfo()
+        {
+            return $"{LastName} {FirstName} {DateOfBirth} {Course} {Group} {Faculty} {University}";
+        }
+    }
+}
