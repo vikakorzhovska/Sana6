43d33dc7ea6a062227299b301bab3eb1096c8d0e
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2024-01-06 12:11:25 +0200

    Created class People

---
+++ Library/People.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class People
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public People(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DateOfBirth = dateOfBirth;
+        }
+        public virtual string ShowInfo()
+        {
+            return $"{LastName} {FirstName} {DateOfBirth}";
+        }
+    } 
+}
