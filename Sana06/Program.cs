61a2f316fe41f4cea21d99f89f7b5f86843dfd61
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2024-01-06 12:30:22 +0200

    Completed all tasks

--- Sana06/Program.cs
+++ Sana06/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Library;
+People[] peoples = new People[5];
+peoples[0] = new People("Viktoria", "Korzhovska", new DateTime(2005, 12, 29));
+peoples[1] = new Applicant("Viktoria", "Korzhovska", new DateTime(2005, 12, 29), 169, 11, "School 2");
+peoples[2] = new Student("Viktoria", "Korzhovska", new DateTime(2005, 12, 29), 1, "IPZ - 23 - 4", "FIKT", "ZTU");
+peoples[3] = new Teacher("Viktoria", "Korzhovska", new DateTime(2005, 12, 29), "Teacher", "Computer Technology", "ZTU");
+peoples[4] = new LibraryUser("Viktoria", "Korzhovska", new DateTime(2005, 12, 29), 238327, new DateTime(2023, 12, 25), 2300);
+for (int i = 0; i < peoples.Length; i++)
+{
+    Console.WriteLine(peoples[i].ShowInfo());
+} 
