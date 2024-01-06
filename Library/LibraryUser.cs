7beb409c1b9f8fe6008ebbfc5c75fb8f0ddbab69
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2024-01-06 12:19:46 +0200

    Created class LibraryUser

---
+++ Library/LibraryUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class LibraryUser : People
+    {
+        public int NumberTicket { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public decimal ReaderContribution { get; set; }
+        public LibraryUser(string firstName, string lastName, DateTime dateOfBirth, int numberTicket, DateTime dateOfIssue, decimal readerContribution) : base(firstName, lastName, dateOfBirth)
+        {
+            NumberTicket = numberTicket;
+            DateOfIssue = dateOfIssue;
+            ReaderContribution = readerContribution;
+        }
+        public override string ShowInfo()
+        {
+            return $"{LastName} {FirstName} {DateOfBirth} {NumberTicket} {DateOfIssue} {ReaderContribution}";
+        }
+    } 
+}
